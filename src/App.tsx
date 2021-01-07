492b0a8f08cdc0de1d8250d69ba9740b95959f39
lazarusWist <lawrence.witt@hotmail.co.uk>
2021-01-07 18:01:52 +0000

    add eslintignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = (): React.ReactElement => {
     const [count, setCount] = React.useState(0);
     const [toggle, setToggle] = React.useState(true);
 
     React.useEffect(() => {
-        setCount(count + 1);
+        setCount(c => c + 1);
     }, [toggle]);
 
     return (
         <>
-            <h1>Hello World! Again</h1>
+            <h1>Hello World!</h1>
             <button onClick={() => setToggle(!toggle)}>Toggle</button>
         </>
     );
-};
+}
 
 export default App;
