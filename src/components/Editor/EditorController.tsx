192e2f1f7a8d09b7eb8049fb12fccc3ed32f10d9
lazarusWist <lawrence.witt@hotmail.co.uk>
2021-02-02 12:15:05 +0000

    add editor panel useTransition

--- src/components/Editor/EditorController.tsx
+++ src/components/Editor/EditorController.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 import { makeStyles } from '@material-ui/core/styles';
 
 import FocusDrawer from '../Drawers/FocusDrawer';
@@ -7,11 +8,14 @@ import CategoryEditor, { CategoryButtons } from './Panels/CategoryEditor';
 import ChooseEditor from './Panels/ChooseEditor';
 import NoteEditor, { NoteButtons } from './Panels/NoteEditor';
 
+type PanelTypes = 'cat' | 'choose' | 'note' | 'rec';
+
 type EditorContentClasses = 
     'categoryEditorContent' | 
     'noteEditorContent';
 
 interface EditorPanel {
+    type: PanelTypes;
     title: string;
     disableGutters: boolean;
     component: string;
@@ -22,6 +26,7 @@ interface EditorPanel {
 
 const panels: Record<string, EditorPanel> = {
     category: {
+        type: 'cat',
         title: "New Category",
         disableGutters: false,
         component: "form",
@@ -30,12 +35,14 @@ const panels: Record<string, EditorPanel> = {
         Content: CategoryEditor
     },
     choose: {
+        type: 'choose',
         title: "Choose New Content",
         disableGutters: true,
         component: "ul",
         Content: ChooseEditor
     },
     note: {
+        type: 'note',
         title: "New Note",
         disableGutters: false,
         component: "div",
@@ -61,18 +68,18 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const EditorController: React.FC = () => {
-    const [panel, setPanel] = React.useState(panels.note);
-
-    const { 
-        title, 
-        disableGutters, 
-        component, 
-        className,
-        Buttons,
-        Content
-    } = panel;
+    const [panel, setPanel] = React.useState(panels.choose);
+    const { title, Buttons } = panel;
 
     const classes = useStyles();
+
+    const panelTransition = useTransition(panel, {
+        key: panel.type,
+        initial: { transform: 'translateX(0%)' },
+        from: { transform: 'translateX(100%)'},
+        enter: { transform: 'translateX(0%)'},
+        leave: { transform: 'translateX(-100%)'}
+    });
 
     return (
         <FocusDrawer open={true}>
@@ -80,13 +87,20 @@ const EditorController: React.FC = () => {
                 {Buttons && <Buttons />}
             </EditorBar>
             <EditorFrame>
-                <EditorContent 
-                    component={component as React.ElementType}
-                    disableGutters={disableGutters}
-                    className={className && classes[className]}
-                >
-                    <Content />
-                </EditorContent>
+                {panelTransition((style, item) => {
+                    const { disableGutters, component, className, Content } = item;
+
+                    return (
+                        <EditorContent
+                            springStyle={style} 
+                            component={component as React.ElementType}
+                            disableGutters={disableGutters}
+                            className={className && classes[className]}
+                        >
+                            <Content />
+                        </EditorContent>
+                    )
+                })}
             </EditorFrame>
         </FocusDrawer>
     )
